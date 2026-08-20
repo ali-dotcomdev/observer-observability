746f526f4a6ef3428bb23757f1564148c793d666
ali <129914644+ali-dotcomdev@users.noreply.github.com>
2026-08-20 13:20:06 +0300

    refactor Cpu Listener

--- src/main/java/com/pipeline/observer/infrastructure/inbound/event/CpuEventListener.java
+++ src/main/java/com/pipeline/observer/infrastructure/inbound/event/CpuEventListener.java
@@ -1,19 +1,28 @@
 package com.pipeline.observer.infrastructure.inbound.event;
 
 import com.pipeline.observer.application.management.event.CpuMetricCreatedEvent;
+import com.pipeline.observer.domain.ports.inbound.usecase.cpu.SaveCpuMetricUseCase;
 import com.pipeline.observer.domain.ports.inbound.usecase.cpu.StreamCpuUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+@Async
 @Component
 @RequiredArgsConstructor
 public class CpuEventListener {
 
     private final StreamCpuUseCase streamCpuUseCase;
+    private final SaveCpuMetricUseCase saveCpu;
 
     @EventListener
-    public void streamCpuMetric(CpuMetricCreatedEvent event){
+    public void streamCpuMetrics(CpuMetricCreatedEvent event){
         streamCpuUseCase.streamCpuMetrics(event.getCpuRecord());
+    }
+
+    @EventListener
+    public void saveCpuMetrics(CpuMetricCreatedEvent event){
+        saveCpu.saveCpuMetrics(event.getCpuRecord());
     }
 }
